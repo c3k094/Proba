576b8d129872eaf64c6af6a23fe32adc599f7094
Tsvetomir Drumev <cvetomirdrumev@mail.bg>
2014-10-15 13:49:52 +0300

    kachvane na papka

---
+++ Proba/main.cpp
@@ -0,0 +1,17 @@
+//zadacha za presmqtane na liceto na pravougulnik//
+#include<iostream>
+using namespace std;
+int main()
+{
+    double a,b;
+    cout<<"vuvedete a "<<endl;
+    cout<<"vuvedete b "<<endl;
+    cin>>a;
+    cin>>b;
+    double p,s;
+    p=2*(a+b);
+    s=a*b;
+    cout<<"p= "<<p<<endl;
+    cout<<"s= "<<s<<endl;
+    return 0;
+}
